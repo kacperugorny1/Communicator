8eef050cdb246ac755d035523b90eaf663c27943
Kacperugorny1 <kacperugorny02@gmail.com>
2025-01-04 10:14:48 +0100

    Simple client server

---
+++ Client/src/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+  int fd, status;
+  char buf[1024];
+  int len;
+  struct addrinfo hints = {.ai_flags = AI_PASSIVE, .ai_socktype = SOCK_STREAM,.ai_family = AF_UNSPEC}
+                , *res, *tmp;
+  //Port check; 
+  if(argc != 2){
+    fprintf(stderr, "usage: ./main port");
+    exit(1);
+  }
+  if(atoi(argv[1]) <= 1024){
+    fprintf(stderr, "port must be numerical port > 1024");
+    exit(2);
+  }
+  
+  //Getting addrinfo
+  if((status = getaddrinfo(NULL, argv[1], &hints, &res) != 0)){
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(3);
+  }
+  
+  //find ipv4
+  tmp = res;
+  while(tmp->ai_family != PF_INET){
+    tmp = tmp->ai_next;
+    if(tmp == NULL) break;
+  }
+  if(tmp == NULL){
+    fprintf(stderr, "Didnt find ipv4\n");
+    exit(4);
+  }
+  
+  //create socket
+  fd = socket(tmp->ai_family, tmp->ai_socktype,tmp->ai_protocol);
+  if(fd == -1){
+    fprintf(stderr, "Couldnt create socket\n");
+    exit(5);
+  }
+
+  puts("Socket created");
+  while((status = connect(fd, tmp->ai_addr, tmp->ai_addrlen)) == -1) ;
+  puts("Connected");
+  len = recv(fd, buf, 1024, 0);
+  if(len == -1) perror("recv");
+  buf[len] = '\0';
+  printf("Message: %s\n", buf);
+}
---
+++ Server/src/main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+  int fd, status, yes = 1;
+  int fd2 = -1;
+  socklen_t addrlen = { 0 };
+  struct addrinfo hints = {.ai_flags = AI_PASSIVE, .ai_socktype = SOCK_STREAM,.ai_family = AF_UNSPEC}
+                , *res, *tmp;
+  struct sockaddr addr ={ 0 };
+  //Port check; 
+  if(argc != 2){
+    fprintf(stderr, "usage: ./main port");
+    exit(1);
+  }
+  if(atoi(argv[1]) <= 1024){
+    fprintf(stderr, "port must be numerical port > 1024");
+    exit(2);
+  }
+  
+  //Getting addrinfo
+  if((status = getaddrinfo(NULL, argv[1], &hints, &res) != 0)){
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(3);
+  }
+  
+  //find ipv4
+  tmp = res;
+  while(tmp->ai_family != PF_INET){
+    tmp = tmp->ai_next;
+    if(tmp == NULL) break;
+  }
+  if(tmp == NULL){
+    fprintf(stderr, "Didnt find ipv4\n");
+    exit(4);
+  }
+  
+  //Create socket bind
+  fd = socket(tmp->ai_family, tmp->ai_socktype,tmp->ai_protocol);
+  if(fd == -1){
+    fprintf(stderr, "Couldnt create socket\n");
+    exit(5);
+  }
+  if(setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&yes, sizeof(yes)) == -1){
+    perror("setsockopt");
+    exit(7);
+  }
+  status = bind(fd, tmp->ai_addr, tmp->ai_addrlen);
+  if(status == -1){
+    fprintf(stderr,"Bind error");
+    exit(6);
+  }
+
+  freeaddrinfo(res);
+
+  if(listen(fd, 10) == -1){
+    fprintf(stderr, "Listen error");
+    exit(10);
+  }
+  puts("Listen");
+
+  while(1){
+    fd2 = accept(fd,&addr,&addrlen);
+    if(fd2 == -1){
+      perror("accept");
+      continue;
+    }
+    puts("Accepted");
+    if(!fork()){
+      printf("Child, fd:%d, fd2:%d\n", fd,fd2);
+      close(fd);
+      if(send(fd2, "Hello world!", 13, 0) == -1) perror("send");
+      close(fd2);
+      puts("child says papa");
+      exit(0);
+    } 
+    close(fd2);
+  }
+  return 0;   
+}
